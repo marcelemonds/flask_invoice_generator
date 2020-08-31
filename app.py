0004719db95ab4435477b609675b2367e1aca69b
Marcel Emonds <marcel.emonds@havasmedia.com>
2020-08-31 23:14:22 +0200

    added bootstrap navbar

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ def create_app():
     @app.route('/')
     def index():
 
-        return render_template('/main.html')
+        return render_template(
+            '/index.html',
+            title='Invoice Generator',
+            active_page='home'
+        )
+
+    @app.route('/invoice_form')
+    def invoice_form():
+
+        return render_template(
+            '/invoice_form.html',
+            title='Invoice Form',
+            active_page='form'
+        )
 
     return app
